614703eb12aa1788b0e92739cdfb83b557ab93ac
Kurt Borgsmiller <45808976+kurborg@users.noreply.github.com>
2019-04-24 15:55:08 -0500

    Update zero_bit_count.c

--- zero_bit_count.c
+++ zero_bit_count.c
@@ -1,42 +1,43 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
 
 int zeroBitCount(int bin)
 {
 
-    int count = 0;
+	int count = 0;
 
-    if (bin == 0)
-        return 1;
+	if (bin == 0)
+		return 1;
 
-    while (bin > 0)
-    {
-        if ((bin & 1) == 0)
-            count = count + 1;
+	while (bin > 0)
+	{
+		if ((bin & 1) == 0)
+			count = count + 1;
 
-        bin = bin >> 1;
-    }
+		bin = bin >> 1;
+	}
 
 
-    return count;
+	return count;
 
 }
 
 int main(int argc, char* argv[])
 {
-    int num;
-    num= atoi(argv[1]);
+	int num;
+	num = atoi(argv[1]);
 
-    if(num < 0)
-    {
-        printf(“Please enter a non-negative integer in the command line \n\n”);
-        return 0;
-    }
+	if (num < 0)
+	{
+		printf("Please enter a non-negative integer \n\n");
+		return 0;
+	}
 
-    printf("\n\nThe number of 0's in %d in binary is %d \n\n", num, zeroBitCount(num));  
+	printf("\n\nThe number of 0's in %d in binary is %d \n\n", num, zeroBitCount(num));
 
 
-    system("pause");
-    return 0;
+	return 0;
 
 }
