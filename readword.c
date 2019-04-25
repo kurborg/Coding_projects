0e156266764e4697b85c67e021500e2e52f60b6a
Kurt Borgsmiller <45808976+kurborg@users.noreply.github.com>
2019-04-25 08:52:03 -0500

    Update readword.c

--- readword.c
+++ readword.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #define WORD_SIZE 50
 
-typedef enum {false  = 0, true = 1} boolean;
 
 // This function will extract tokens from file - one word at a time
 // A word is defined by a sequence of non-whitespace characters surrounded
@@ -14,19 +13,36 @@ typedef enum {false  = 0, true = 1} boolean;
 
 char* get_token(FILE* fp)
 {
-    char* token = calloc(WORD_SIZE, sizeof(char));
+	char* token = calloc(WORD_SIZE, sizeof(char));
+	char ch;
+	int fileStatus, i = 0;
 
-    fgets(token, WORD_SIZE, fp);
+	while (1)
+	{
+		ch = fgetc(fp);
 
-    // reached end of file -->
-    if (fgets(token, WORD_SIZE, fp) == NULL)
-    {
-    return NULL;
-    }
+		if (ch == EOF)
+		{
+			fileStatus = 0;
+			break;
+		}
 
-    // Otherwise return token
-    printf("\nRaw token = %s",token);
-    return token;
+		else if (ch == '\n' || ch == ' ' || ch == '\t')
+		{
+			token[i] = '\0';
+			i = 0;
+			printf("\nRaw token = %s",token);
+			return token;
+			break;
+		}
+
+		else
+			token[i++] = ch;
+		}
+
+    	if(fileStatus == 0) 
+		return NULL;
+  
 }
 
 
@@ -45,7 +61,7 @@ int main(int argc, char *argv[])
   {
       ++tokens_count;
 
-      printf("Token: %s", word);
+      printf("\nToken: %s", word);
 
       free(word);
   }
