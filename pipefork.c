3228e157159286a0013fc6eaff0beb40fff021f3
Kurt Borgsmiller <45808976+kurborg@users.noreply.github.com>
2019-05-01 15:31:10 -0500

    Update pipefork.c

--- pipefork.c
+++ pipefork.c
@@ -15,9 +15,10 @@ int main(int argc, char* argv[])
     exit(1);
   }
   
-  int numArgs = argc;
   int pipes[4][2];
-  int wait_status[numArgs];
+  int wait_status[argc];
+  int write_status = 1;
+  int read_status = 1;
   
   pipe(pipes);
   
@@ -29,26 +30,32 @@ int main(int argc, char* argv[])
   {
     children[i] = fork();
     
+    //CHILD PROCESSES
     if(children[i] == 0)
 	{
 	     for(int j = 0; j <argc; j++)
 	     {
 		if (i == j)
-		   break;
-		     
+		   close(pipes[j][0]);
+		else
+		{
 		close(pipes[j][0]);
 		close(pipes[j][1]);
+		}
 	     }
+	    
+	    
 	    	dup2(pipe_ends[i], 1);
 		execlp(argv[i+1], "ls", "-l", NULL);
+	       //ANYTHING AFTER EXEC ONLY RUNS IN EXEC FAILED!
+	   	printf("\nExec failed!");
+	    
+	    
 		char buffer;
-		int read_status = 1;
-		int write_status = 1;
 
-		while(read_status > 0)
+		while(write_status > 0)
 		{
-			read_status = read(input_fd, &buffer, 1);
-			write_status = write(pipe_ends[i], &buffer, 1);
+			write_status = write(pipes[i][1], &buffer, 1);
 
 			if(write_status < 0)
 			{
@@ -57,33 +64,33 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		if(read_status < 0)
-		{
-			fprintf(stderr, "One of the read() calls failed.\n\n");
-			return EXIT_FAILURE;
-		}
-
 		printf("child %d has finished passing the input file into the pipe.\n\n", i+1);
 		return EXIT_SUCCESS;
 	}
-	//fork() error
+	  
+	//PARENT PROCESS
+	else if(children[i] > 0)
+	{
+		for(int j = Homework 30; j <argc; j++)
+	        {
+			close(pipes[j][1]);
+		}
+		
+		read_status = read(pipes[i][0], &buffer, 1);
+
+		waitpid(children[i], &wait_status[i], 0);
+		printf("Child %d has been collected.\n\n" , i+1);
+
+	}
+	  
+	  
+	//FORK() ERROR
 	else if(children[i] < 0)
 	{
 		fprintf(stderr, "The first fork() call failed.\n\n");
 		return EXIT_FAILURE;
 	}  
 	  
-	else if(children[i] > 0)
-	{
-		for(int j = 0; j <argc; j++)
-	        {
-			close(pipes[j][1]);
-		}
-
-		waitpid(child[i], &wait_status[i], 0);
-		printf("Child %d has been collected.\n\n" , i+1);
-
-	}
   
   }
   	
@@ -92,3 +99,15 @@ printf("All children have been collected. Program exiting\n\n");
 return EXIT_SUCCESS;
 
 }
+
+
+
+/*
+NOTES FROM CLASS
+
+for (int i = 0; i < argc; i++)
+	close(pipe[i][1];
+
+
+
+*/
