1d94c8184bf5d2513f6245617101fdbc14a70d16
Kurt Borgsmiller <45808976+kurborg@users.noreply.github.com>
2019-05-02 15:29:28 -0500

    Update pipefork.c

--- pipefork.c
+++ pipefork.c
@@ -14,23 +14,23 @@ int main(int argc, char* argv[])
     printf("ERROR: Incorrect number of file arguments!\n");
     exit(1);
   }
-  
+
   int pipes[4][2];
   int wait_status[argc];
   int write_status[argc] = 1;
   int read_status[argc] = 1;
   char buffer;
-  
+
   pipe(pipes);
-  
+
   pid_t children[numArgs];
-	
-  
-  
+
+
+
   for(int i = 0; i <argc; i++)
   {
     children[i] = fork();
-    
+
     //CHILD PROCESSES
     if(children[i] == 0)
 	{
@@ -44,20 +44,20 @@ int main(int argc, char* argv[])
 		close(pipes[j][1]);
 		}
 	     }
-	    
-	    
-	    	dup2(pipes[i][1], 1);
-		execlp(argv[i+1],argv[i+1],"ls", "-l", NULL);
+
+
+        dup2(pipes[i][1], 1);
+		execlp("ls", "ls", "-l", argv[i+1], NULL);
 	       //ANYTHING AFTER EXEC ONLY RUNS IN EXEC FAILED!
 	   	printf("\nExec failed!");
-	
+
 	}
-	  
+
 	//PARENT PROCESS
 	else if(children[i] > 0)
 	{
 		close(pipes[i][1]);
-		
+
 		while(read_status[i] > 0)
 		{
 			read_status[i] = read(pipes[i][0], &buffer, 1);
@@ -69,32 +69,32 @@ int main(int argc, char* argv[])
 				return EXIT_FAILURE;
 			}
 		}
-		
+
 		if(read_status[i] < 0)
 		{
 			fprintf(stderr, "read() call %d failed.\n\n", i+1);
 			return EXIT_FAILURE;
 		}
-		
-		
+
+
 
 		waitpid(children[i], &wait_status[i], 0);
 		printf("Child %d has been collected.\n\n" , i+1);
 
 	}
-	  
-	  
+
+
 	//FORK() ERROR
 	else if(children[i] < 0)
 	{
 		fprintf(stderr, "fork() %d call failed.\n\n", i+1);
 		return EXIT_FAILURE;
-	}  
-	  
-  
+	}
+
+
   }
-  	
-    
+
+
 printf("All children have been collected. Program exiting\n\n");
 return EXIT_SUCCESS;
 
@@ -104,10 +104,6 @@ return EXIT_SUCCESS;
 
 /*
 NOTES FROM CLASS
-
 for (int i = 0; i < argc; i++)
 	close(pipe[i][1];
-
-
-
 */
